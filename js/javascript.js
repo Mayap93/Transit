f54eb4aa7896ffff127e3dab363fbc86faf9adde
Mayap93 <36158705+Mayap93@users.noreply.github.com>
2018-02-20 13:16:09 +0100

    box number two

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,9 @@
 $('.box').mouseover(function(){
 $('.box').transition({ scale: [2.0, 1.5] });
 });
+
+
+$('.boxTwo').mouseover(function(){
+  $('.boxTwo').transition({perspective: '100px',
+  rotate3d: '1,1,0,180deg'})
+})
