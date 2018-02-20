263355db39e83989e2d25cbf7f39f0d34212d3d1
Mayap93 <36158705+Mayap93@users.noreply.github.com>
2018-02-20 13:19:39 +0100

    normal

--- js/javascript.js
+++ js/javascript.js
@@ -5,8 +5,3 @@ $('.box').transition({ scale: [2.0, 1.5] });
 $('.box').mouseout(function(){
   $('.box').transition({ scale: [1, 1] });
 });
-
-$('.boxTwo').mouseover(function(){
-  $('.boxTwo').transition({perspective: '100px',
-  rotate3d: '1,1,0,180deg'})
-})
