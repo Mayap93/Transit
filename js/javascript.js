0f1c64890bfb83378137a4b514df68e95def1ccb
joannamucha <33253378+joannamucha@users.noreply.github.com>
2018-02-20 13:38:04 +0100

    random number and transitions

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,15 @@ $('.box').transition({ scale: [2.0, 1.5] });
 $('.box').mouseout(function(){
   $('.box').transition({ scale: [1, 1] });
 });
+
+
+var randomNumber = Math.floor((Math.random() * 5) + 1);
+document.getElementById('button').onclick = function(){
+
+  if (randomNumber = 1) {
+    $('.one').transition({ scale: [2.0, 1.5] });
+  }
+  if (randomNumber = 2) {
+$('.two').transition({ scale: [5.0, 1.5] });
+  }
+}
